b23bb3e991724aec38cea1f07796ba7e695f11a9
fridayfang <fridayfang@zhaoxin.com>
2020-07-13 18:38:10 +0800

    refine gstreamer 11.
    Signed-off-by: fridayfang <fridayfang@zhaoxin.com>

--- gstreamer-exer/make.sh
+++ gstreamer-exer/make.sh
@@ -1,4 +1,6 @@
-gcc tutorial09.c -o abc `pkg-config --cflags --libs gstreamer-1.0 gstreamer-audio-1.0`
+#gcc tutorial09.c -o abc `pkg-config --cflags --libs gstreamer-1.0 gstreamer-audio-1.0`
+gcc tutorial10.c -o abc `pkg-config --cflags --libs gstreamer-1.0`
+
 #终端敲 gcc tutorial01.c -o abc `pkg-config --cflags --libs gstreamer-1.0` 自动转换为下面:
 #gcc tutorial09.c -o abc -pthread -I/usr/local/include/gstreamer-1.0 -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -L/usr/local/lib -lgstreamer-1.0 -lgobject-2.0 -lglib-2.0
 
--- gstreamer-exer/tutorial11/tutorial11-qtoverlay.cpp
+++ gstreamer-exer/tutorial11/tutorial11-qtoverlay.cpp
@@ -18,10 +18,10 @@ PlayerWindow::PlayerWindow(GstElement *p) : pipeline(p), state(GST_STATE_NULL), 
     slider = new QSlider(Qt::Horizontal);
     timer = new QTimer();
 
-    connect(playBt, SIGNAL(clicked()), this, SLOT(onPlayClicked()));
+    connect(playBt, SIGNAL(clicked()), this, SLOT(onPlayClicked()));    //playBt对象可能发送clicked信号,触发onPlayClicked函数
     connect(pauseBt, SIGNAL(clicked()), this, SLOT(onPauseClicked()));
     connect(stopBt, SIGNAL(clicked()), this, SLOT(onStopClicked()));
-    connect(slider, SIGNAL(sliderReleased()), this, SLOT(onSeek()));
+    connect(slider, SIGNAL(sliderReleased()), this, SLOT(onSeek()));    //slider对象可能发送sliderReleased信号,触发onSeek函数
 
     buttonLayout = new QHBoxLayout;
     buttonLayout->addWidget(playBt);
@@ -35,8 +35,8 @@ PlayerWindow::PlayerWindow(GstElement *p) : pipeline(p), state(GST_STATE_NULL), 
 
     this->setLayout(playerLayout);
 
-    connect(timer, SIGNAL(timeout()), this, SLOT(refreshSlider()));
-    connect(this, SIGNAL(sigAlbum(QString)), this, SLOT(onAlbumAvaiable(QString)));
+    connect(timer, SIGNAL(timeout()), this, SLOT(refreshSlider())); //timer对象可能会发送 timerout 信号,触发 refreshSlider 函数
+    connect(this, SIGNAL(sigAlbum(QString)), this, SLOT(onAlbumAvaiable(QString))); //this对象可能触发sigAlbum信号(携带QString参数)
     connect(this, SIGNAL(sigState(GstState)), this, SLOT(onState(GstState)));
     connect(this, SIGNAL(sigEos()), this, SLOT(onEos()));
 }
@@ -45,11 +45,19 @@ WId PlayerWindow::getVideoWId() const {
     return videoWindow->winId();
 }
 
+//当播放结束或点击Stop时,GStreamer会在状态切换到NULL时释放所有资源.所以我们在此需要重新设置playbin的vido-sink，并指定视频输出窗口
 void PlayerWindow::onPlayClicked() {
     GstState st = GST_STATE_NULL;
     gst_element_get_state (pipeline, &st, NULL, GST_CLOCK_TIME_NONE);
     if (st < GST_STATE_PAUSED) {
-        ///////g_object_get(GST_OBJECT(pipeline), "video-sink", vsink, NULL);
+        GstElement *video_sink;
+        g_object_get(GST_OBJECT(pipeline), "video-sink", &video_sink, NULL);
+        if (video_sink) {
+            g_print("111\n");
+        } else {
+            g_print("000\n");
+        }
+
         // Pipeline stopped, we need set overlay again
         GstElement *vsink = gst_element_factory_make ("ximagesink", "vsink");
         g_object_set(GST_OBJECT(pipeline), "video-sink", vsink, NULL);
@@ -63,7 +71,7 @@ void PlayerWindow::onPauseClicked() {
     gst_element_set_state (pipeline, GST_STATE_PAUSED);
 }
 
-void PlayerWindow::onStopClicked() {
+void PlayerWindow::onStopClicked() {//当播放结束或点击Stop时,GStreamer会在状态切换到NULL时释放所有资源.
     gst_element_set_state (pipeline, GST_STATE_NULL);
 }
 
@@ -71,11 +79,11 @@ void PlayerWindow::onAlbumAvaiable(const QString &album) {
     setWindowTitle(album);
 }
 
-void PlayerWindow::onState(GstState st) {
+void PlayerWindow::onState(GstState st) {   //状态转换 启停timer刷新
     if (state != st) {
         state = st;
         if (state == GST_STATE_PLAYING) {
-            timer->start(1000);
+            timer->start(1000); //1s定时,timeout 触发 refreshSlider()
         }
         if (state < GST_STATE_PAUSED) {
             timer->stop();
@@ -87,11 +95,11 @@ void PlayerWindow::refreshSlider() {
     gint64 current = GST_CLOCK_TIME_NONE;
     if (state == GST_STATE_PLAYING) {
         if (!GST_CLOCK_TIME_IS_VALID(totalDuration)) {
-            if (gst_element_query_duration (pipeline, GST_FORMAT_TIME, &totalDuration)) {
+            if (gst_element_query_duration (pipeline, GST_FORMAT_TIME, &totalDuration)) {//query总时长
                 slider->setRange(0, totalDuration/GST_SECOND);
             }
         }
-        if (gst_element_query_position (pipeline, GST_FORMAT_TIME, &current)) {
+        if (gst_element_query_position (pipeline, GST_FORMAT_TIME, &current)) { //query当前时间
             g_print("%ld / %ld\n", current/GST_SECOND, totalDuration/GST_SECOND);
             slider->setValue(current/GST_SECOND);
         }
@@ -108,6 +116,8 @@ void PlayerWindow::onEos() {
     gst_element_set_state (pipeline, GST_STATE_NULL);
 }
 
+//在postGstMessage中我们没有直接更新GUI,因为GStreamer的Bus处理线程与GUI主线程可能为不同线程
+//直接更新GUI会出错或无效. 因此利用Qt的signal-slot机制在相应的槽函数中就行GUI信息的更新
 gboolean PlayerWindow::postGstMessage(GstBus * bus, GstMessage * message, gpointer user_data) {
     PlayerWindow *pw = NULL;
     if (user_data) {
@@ -117,7 +127,7 @@ gboolean PlayerWindow::postGstMessage(GstBus * bus, GstMessage * message, gpoint
         case GST_MESSAGE_STATE_CHANGED: {
             GstState old_state, new_state, pending_state;
             gst_message_parse_state_changed (message, &old_state, &new_state, &pending_state);
-            pw->sigState(new_state);
+            pw->sigState(new_state);    //将gstreamer消息转化为qt的信号
             break;
         }
         case GST_MESSAGE_TAG: {
@@ -157,9 +167,9 @@ int main(int argc, char *argv[])
 
     // seg window id to gstreamer
     GstElement *vsink = gst_element_factory_make ("ximagesink", "vsink");
+    g_object_set(GST_OBJECT(pipeline), "video-sink", vsink, NULL);              //使用g_object_set()将自定义的Sink通过“video-sink”属性设置到playbin中
     WId xwinid = window->getVideoWId();
-    gst_video_overlay_set_window_handle (GST_VIDEO_OVERLAY (vsink), xwinid);
-    g_object_set(GST_OBJECT(pipeline), "video-sink", vsink, NULL);
+    gst_video_overlay_set_window_handle (GST_VIDEO_OVERLAY (vsink), xwinid);    //将Qt创建的视频窗口ID设置给GStreamer,让GStreamer得到渲染的窗口ID
 
     // connect to interesting signals
     GstBus *bus = gst_element_get_bus(pipeline);
@@ -175,7 +185,9 @@ int main(int argc, char *argv[])
         QTimer::singleShot(0, QApplication::activeWindow(), SLOT(quit()));
     }
 
-    int ret = app.exec();
+    int ret = app.exec();   //调用GUI框架的事件循环,exec()会一直执行,直到关闭窗口.这是替代了gstreamer自有的loop循环
+    //由于GStreamer的GstBus会默认使用GLib的主循环及事件处理机制,所以必须要保证GLi默认的MainLoop在某个线程中运行
+    //在本例中,Qt在Linux下会自动使用GLib的主循环,所以无需额外进行处理
 
     window->hide();
     gst_element_set_state (pipeline, GST_STATE_NULL);
